79086fd887308acf829503ad97cc6a99fd5bf95f
i459452 <459452@student.fontys.nl>
2025-04-11 17:12:18 +0200

    Fixing a test

--- user_service/app/src/test/java/backend2/business/user/AddUserUseCaseTest.java
+++ user_service/app/src/test/java/backend2/business/user/AddUserUseCaseTest.java
@@ -107,7 +107,7 @@ class AddUserUseCaseTest {
     void createUser_WithNullInput_ShouldReturnNull() {
         // Arrange
         when(userMapper.toEntity(null)).thenReturn(null);
-        when(userRepository.save(null)).thenReturn(null);
+        // We don't need to mock save(null) since toEntity returns null
         when(userMapper.toDTO(null)).thenReturn(null);
 
         // Act
@@ -118,7 +118,7 @@ class AddUserUseCaseTest {
 
         // Verify interactions
         verify(userMapper, times(1)).toEntity(null);
-        verify(userRepository, times(1)).save(null);
-        verify(userMapper, times(1)).toDTO(null);
+        verify(userRepository, never()).save(any());  // save should never be called
+        verify(userMapper, never()).toDTO(any());  // toDTO should never be called
     }
 } 
